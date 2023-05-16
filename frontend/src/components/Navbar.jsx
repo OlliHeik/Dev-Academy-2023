c17613c70c53109a68af7bd74a258f0b38fe0930
OlliHeik <olli.heikkinen@telemail.fi>
2023-05-16 21:18:44 +0300

    logo, navbar and footer design

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import Logo from "../img/logo.png"
+import {Link} from "react-router-dom"
 
 const Navbar = () => {
     return (
-        <div>Navbar</div>
+        <div className="navbar">
+            <div className="container">
+                <div className="logo">
+                    <img src={Logo} alt=""/>
+                </div>
+                <div className="links">                    
+                    <Link className="link">
+                        <h6>LINKS</h6>
+                    </Link>
+                    <Link className="link">
+                        <h6>LINKS</h6>
+                    </Link>
+                    <Link className="link">
+                        <h6>LINKS</h6>
+                    </Link>
+                    <Link className="link">
+                        <h6>LINKS</h6>
+                    </Link>
+                    <span className="add">
+                        <Link className="add-link">ADD JOURNEY</Link>
+                    </span>
+                </div>
+            </div>
+        </div>
     )
 }
 
